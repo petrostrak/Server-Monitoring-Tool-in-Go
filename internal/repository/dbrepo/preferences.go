febebb8b05b6e655f1622536b7a9fe05962fa550
petrostrak <pit.trak@gmail.com>
2021-09-24 22:46:53 +0300

    Monitoring

--- internal/repository/dbrepo/preferences.go
+++ internal/repository/dbrepo/preferences.go
@@ -86,3 +86,19 @@ func (m *postgresDBRepo) InsertOrUpdateSitePreferences(pm map[string]string) err
 
 	return nil
 }
+
+// UpdateSystemPref updates a system preference setting
+func (m *postgresDBRepo) UpdateSystemPref(name, value string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := `update preferences set preference = $1, updated_at = $2 where name = $3`
+
+	_, err := m.DB.ExecContext(ctx, query, value, time.Now(), name)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	return nil
+}
