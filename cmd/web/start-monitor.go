a23b997f48b83f6033533b7fe713b628a4a9c9ea
petrostrak <pit.trak@gmail.com>
2021-09-24 22:14:36 +0300

    Monitoring

--- cmd/web/start-monitor.go
+++ cmd/web/start-monitor.go
@@ -1,12 +1,17 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+)
 
 type job struct {
 	HostServiceID int
 }
 
-func (j *job) Run() {
+func (j job) Run() {
 	repo.ScheduledCheck(j.HostServiceID)
 }
 
@@ -29,16 +34,57 @@ func startMonitoring() {
 		log.Println("length of services to monitor", len(servicesToMonitor))
 
 		// range through the services
+		for _, x := range servicesToMonitor {
+			// get the schedule unit and number
+			var sch string
+			if x.ScheduleUnit == "d" {
+				sch = fmt.Sprintf("@every %d%s", x.ScheduleNumber*24, "h")
+			} else {
+				sch = fmt.Sprintf("@evert %d%s", x.ScheduleNumber, x.ScheduleUnit)
+			}
 
-		// get the schedule unit and number
+			// create a job
+			var j job
+			j.HostServiceID = x.ID
+			scheduleID, err := app.Scheduler.AddJob(sch, j)
+			if err != nil {
+				log.Println(err)
+			}
 
-		// create a job
+			// save the id of the job so we can start and stop it
+			app.MonitorMap[x.ID] = scheduleID
 
-		// save the id of the job so we can start and stop it
+			// broadcast over websockets the fact that se service
+			// is scheduled
+			payload := make(map[string]string)
+			payload["message"] = "scheduling"
+			payload["host_service_id"] = strconv.Itoa(x.ID)
+			yearOne := time.Date(0001, 11, 17, 20, 34, 58, 68138737, time.UTC)
+			if app.Scheduler.Entry(app.MonitorMap[x.ID]).Next.After(yearOne) {
+				payload["next_run"] = app.Scheduler.Entry(app.MonitorMap[x.ID]).Next.Format("2006-01-02 3:04:05 PM")
+			} else {
+				payload["next_run"] = "Pending..."
+			}
 
-		// broadcast over websockets the fact that se service
-		// is scheduled
+			payload["host"] = x.HostName
+			payload["service"] = x.Service.ServiceName
+			if x.LastCheck.After(yearOne) {
+				payload["last_run"] = x.LastCheck.Format("2006-01-02 3:04:05 PM")
+			} else {
+				payload["last_run"] = "Pending..."
+			}
+			payload["schedule"] = fmt.Sprintf("@every %d%s", x.ScheduleNumber, x.ScheduleUnit)
 
-		// end range
+			err = app.WsClient.Trigger("public-channel", "next-run-event", payload)
+			if err != nil {
+				log.Println(err)
+			}
+
+			err = app.WsClient.Trigger("public-channel", "schedule-changed-event", payload)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+
 	}
 }
